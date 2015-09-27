5a21e42f78864cc0b844a9f7eb0687e266a3de01
Ari Vaniderstine <hack@ariari.io>
2015-09-27 11:36:14 -0400

    new gif

--- visualizer.js
+++ visualizer.js
@@ -1,13 +1,12 @@
-alert("file opened");
 var div = document.createElement('div');
 div.style.width = '100vw';
 div.style.height = '100vh';
-div.style.backgroundImage = 'url(http://i.giphy.com/juy2vR1O234yI.gif)';
+div.style.background = 'url(http://cdn.makeagif.com/media/9-27-2015/RJaBlZ.gif)';
 div.style.backgroundSize = 'cover';
 div.style.zIndex = '10000';
 div.style.position = 'absolute';
 div.style.top = '0';
 div.style.left = '0';
-div.className = "visualizer";
+div.className = 'visualizer';
 
 document.body.appendChild(div);
