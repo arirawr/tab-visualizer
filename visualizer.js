9d5efd655b656faf7608ef40e9af6e29ded62c23
Ari Vaniderstine <hack@ariari.io>
2015-09-27 11:42:07 -0400

    resize on click

--- visualizer.js
+++ visualizer.js
@@ -4,9 +4,18 @@ div.style.height = '100vh';
 div.style.background = 'url(http://cdn.makeagif.com/media/9-27-2015/RJaBlZ.gif)';
 div.style.backgroundSize = 'cover';
 div.style.zIndex = '10000';
-div.style.position = 'absolute';
+div.style.position = 'fixed';
 div.style.top = '0';
 div.style.left = '0';
 div.className = 'visualizer';
 
 document.body.appendChild(div);
+
+div.addEventListener('click', function() {
+	if(div.style.height === '100vh') {
+		div.style.height = '5vh';
+	}
+	else {
+		div.style.height = '100vh';
+	}
+})
